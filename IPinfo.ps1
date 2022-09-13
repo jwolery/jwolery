bcf4f0f3edf5d43a3d113174949241389a880391
Justin Wolery <justin@pcgamerzhawaii.com>
2022-09-12 18:48:02 -1000

    Optimized/removed redundant code, GEOLocation Data is now output in sentances.

--- IPinfo.ps1
+++ IPinfo.ps1
@@ -5,99 +5,68 @@ $ipcheck = ($a -as [IPaddress]) -as [Bool]
 if($ipcheck)
 {
     ''
-    write-host "Thanks! Initial validation check thinks this is a Valid ip address, if its not responsive we'll know shortly."
+    write-host "Thanks! I will check if this IP ping-able however if its not responsive I won't be able to provide any info."
     ''
-    write-host "Please give me a moment to fetch available Geo Location info and calculate an average of 10 Pings"
-}
-elseif($a -like "*/*" -or $a -like "*-*")
-{
-    $cidr = $a.split("/")
-    if($cidr[1] -ge '0' -and $cidr[1] -le '32')
-    {
-        write-host "Ok, that looks like a valid subnet"
-    }
-    elseif($a -like "*-*")
-    {
-        $ip = $a.split("-")
-        $ip1 = $ip[0] -as [IPaddress] -as [Bool]
-        $ip2 = $ip[1] -as [IPaddress] -as [Bool]
-        if($ip -and $ip)
-        {
-            write-host "Ok, that looks like a valid ip address range"
-        }
-        else
-        {
-            write-host "invalid range"
-        }
- 
-    }
-    else {
-        write-host "invalid subnet"
-    }
+    write-host "Please give me a moment to validate, fetch available Geo Location info and calculate an average of 10 Pings"
 }
 else
 {
     write-host "Sorry, that is not a valid address"
     $IPAddress = Read-Host -Prompt "Please re-enter the IP address"
+    ''
+    write-host "Thanks! I will check if this IP ping-able however if its not responsive I won't be able to provide any info."
+    ''
+    write-host "Please give me a moment to validate, fetch available Geo Location info and calculate an average of 10 Pings"
 }
 try {
     $Ping = Test-Connection -Count 10 -ComputerName $IPAddress
     }
 catch {
-    Write-Host "Looks like this IP is either not responding to Pings or possibly Invalid." -ForegroundColor RED
+    Write-Host "ERROR: Looks like this IP is either not responding to Pings or possibly Invalid." -ForegroundColor RED
     Exit
-}
-finally {
-    $Error.Clear()
 }
 $Avg = ($Ping | Measure-Object ResponseTime -average)
 $Calc = [System.Math]::Round($Avg.average)
-if ($Calc -eq 0) {
-    <# Action to perform if the condition is true #>
-}
-elseif ($Calc -gt 1) {
+if ($Calc -gt 1) {
     <# Action when this condition is true #>
     try {
         ''
-        Write-Host 'Thanks for waiting, Here is the GeoIP Data:' -ForegroundColor Green
+        Write-Host 'SUCCESS! Thanks for waiting, Here is what I found:' -ForegroundColor Green
         Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$IPAddress" -ErrorAction SilentlyContinue | Foreach-object {
-            [pscustomobject]@{
-            Country       = $_.Country
-            CountryCode   = $_.CountryCode
-            Region        = $_.Region
-            City          = $_.City
-            'Postal Code' = $_.Zip
-            'Network Org' = $_.Org
-            ISP           = $_.ISP
-            AS            = $_.as
-            Lat           = $_.Lat
-            Lon           = $_.Lon
-            TimeZone      = $_.TimeZone
-        }
         $IPCity = [PSCustomObject]$_.City
         $IPRegion = [PSCustomObject]$_.Region
         $IPCountry = [PSCustomObject]$_.Country
         $IPtz = [PSCustomObject]$_.TimeZone
+        $IPLat = [PScustomObject]$_.Lat
+        $IPLon = [PScustomObject]$_.Lon
+        $IPZip = [PScustomObject]$_.Zip
+        $IPOrg = [PScustomObject]$_.Org
+        $IPISP = [PScustomObject]$_.ISP
+        $IPAS = [PScustomObject]$_.as
     }
-    "The average response time to $IPAddress is $Calc ms and is located in $IPCity, $IPRegion in $IPCountry `n"
-    Write-Host 'Here is todays weather around that area:' -ForegroundColor Blue
+    ''
+    Write-Host "The average latency to $IPAddress is $Calc ms and is located in $IPCity, $IPRegion in $IPCountry." -ForegroundColor DarkYellow
+    ''
+    Write-Host "The reported Latitude & Longitude is $IPLat,$IPLon and the postal code is $IPZip." -ForegroundColor DarkYellow
+    ''
+    Write-Host "I am showing the Netblock Org/ISP as $IPOrg/$IPISP and the AS number is $IPAS." -ForegroundColor DarkYellow
+    ''
     }
     catch {
     Write-Warning -Message "$IPAddress : $_"
     }
 }
-# Get the Weather for the Geolocation Data
-
-(curl wttr.in/"$IPCity,$IPRegion"?0 -UserAgent "curl" ).Content
-
 # Get the current datetime of the IP
-
 Invoke-RestMethod -Method Get -Uri "http://worldtimeapi.org/api/ip/$IPAddress.json" -ErrorAction SilentlyContinue | Foreach-object {
-    [PSCustomObject]@{
-    }
-    $IPTime = $_.datetime
+    $IPTime = [PSCustomObject]$_.datetime
     Write-Host "$IPCity is in the $IPtz timezone where it is" -ForegroundColor Cyan ([DateTimeOffset] $_.datetime).ToString('f')
 }
+# Get the Weather for the Geolocation Data
+''
+Write-Host 'Here is todays weather around that area:' -ForegroundColor Blue
+''
+(curl wttr.in/"$IPCity,$IPRegion"?0 -UserAgent "curl" ).Content
+
 # reset variables for next run
 $Calc = $null
 $Avg = $null
